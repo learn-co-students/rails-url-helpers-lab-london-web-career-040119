e4d3c227003a8ebf4d48904109d386a9121ba25f
ellevalentine <elle.valentine@hotmail.com>
2019-04-27 19:25:26 +0100

    not finished

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,28 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
-  def index
+  before_action :set_student, only: :show	  before_action :set_student, only: :show
+
+   def index	  def index
     @students = Student.all
   end
 
-  def show
+
+   def show	  def show
+    @student = Student.find(params[:id])
+    @active_status = @student.active ? 'active' : 'inactive'
   end
 
-  private
+   def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+
+   private	  private
+
+
+     def set_student	  def set_student
+      @student = Student.find(params[:id])	    @student = Student.find(params[:id])
+    end	  
 end
