a46b48fc3d765f474f3d3a91f6971f0f9ff1820b
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-27 22:26:33 +0100

    Add routes for user to navigate & add activate student attribute with toggle feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,12 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
-    @student.active = !@student.active
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+
     @student.save
     redirect_to student_path(@student)
   end
