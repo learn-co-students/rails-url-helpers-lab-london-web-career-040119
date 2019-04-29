7385d877ac02e333d022d7f8163b52380ea8e0ca
hlbtyne <harriet.ballantyne@gmail.com>
2019-04-29 14:15:23 +0100

    Lab passed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    if set_student.active
+      set_student.active = false
+    else
+      set_student.active = true
+    end
+
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
